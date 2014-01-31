3c10d00da0367155a723dd445e88659c9b748293
Torjus Iveland <torjusti@gmail.com>
2014-01-31 22:21:04 +0100

    Create gulp config file.

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var coffee = require('gulp-coffee');
+var rename = require('gulp-rename');
+
+gulp.task('default', function() {
+  gulp.src('src/selix.coffee')
+    .pipe(coffee())
+    .pipe(gulp.dest('lib'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify({preserveComments: 'some'}))
+    .pipe(gulp.dest('lib'))
+});
