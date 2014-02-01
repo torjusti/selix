12d45ffb69e7b9f572addbf3a88e73f7475209d7
Torjus Iveland <torjusti@gmail.com>
2014-02-01 13:45:20 +0100

    Makefile to fetch a copy of the current selix from the master branch.

---
+++ Makefile
@@ -0,0 +1,3 @@
+all:
+	git show master:lib/selix.js > dist/js/selix.js
+	git show master:lib/selix.min.js > dist/js/selix.min.js
---
+++ dist/js/selix.js
@@ -0,0 +1,94 @@
+
+/* @license selix - github.com/bilde - MIT License */
+
+(function() {
+  var getCaret, getText, lineHandler, selix, setCaret, setText, wrap;
+
+  lineHandler = function(elem, position) {
+    return elem.value.slice(0, position).split('\r\n').length - 1;
+  };
+
+  getCaret = function(elem) {
+    var elemRange, end, endRange, start;
+    if (typeof elem.selectionStart === 'number' && typeof elem.selectionEnd === 'number') {
+      return {
+        start: elem.selectionStart,
+        end: elem.selectionEnd
+      };
+    } else if (document.selection && document.selection.createRange && elem.createTextRange) {
+      elemRange = elem.createTextRange();
+      elemRange.moveToBookmark(document.selection.createRange().getBookmark());
+      endRange = elem.createTextRange();
+      endRange.collapse(false);
+      if (elemRange.compareEndPoints('StartToEnd', endRange) >= 0) {
+        start = end = elem.value.length;
+      } else {
+        start = -elemRange.moveStart('character', -elem.value.length);
+        start += lineHandler(elem, start);
+        if (elemRange.compareEndPoints('EndToEnd', endRange) >= 0) {
+          end = elem.value.length;
+        } else {
+          end = -elemRange.moveEnd('character', -elem.value.length);
+          end += lineHandler(elem, end);
+        }
+      }
+      return {
+        start: start,
+        end: end
+      };
+    }
+  };
+
+  setCaret = function(elem, start, end) {
+    var range;
+    if (end == null) {
+      end = start;
+    }
+    if (typeof elem.selectionStart === 'number' && typeof elem.selectionEnd === 'number') {
+      elem.selectionStart = start;
+      return elem.selectionEnd = end;
+    } else if (document.selection && document.selection.createRange && elem.createTextRange) {
+      range = elem.createTextRange();
+      range.collapse(true);
+      range.moveEnd('character', end - lineHandler(elem, end));
+      range.moveStart('character', start - lineHandler(elem, start));
+      return range.select();
+    }
+  };
+
+  getText = function(elem) {
+    var caret;
+    caret = getCaret(elem);
+    return elem.value.slice(caret.start, caret.end);
+  };
+
+  setText = function(elem, text) {
+    var caret;
+    caret = getCaret(elem);
+    return elem.value = elem.value.slice(0, caret.start) + text + elem.value.slice(caret.end);
+  };
+
+  wrap = function(elem, before, after) {
+    var caret, startPosition, text;
+    caret = getCaret(elem);
+    text = getText(elem);
+    setText(elem, before + text + after);
+    startPosition = caret.start + before.length;
+    return setCaret(elem, startPosition, startPosition + text.length);
+  };
+
+  selix = {
+    getCaret: getCaret,
+    setCaret: setCaret,
+    getText: getText,
+    setText: setText,
+    wrap: wrap
+  };
+
+  if (typeof exports !== 'undefined') {
+    module.exports = selix;
+  } else if (typeof window !== 'undefined') {
+    window.selix = selix;
+  }
+
+}).call(this);
